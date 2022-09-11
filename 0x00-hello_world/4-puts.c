0692d5ae9faa86a85ca094b6bece3f85d2435008
Pangani <roypangani@gmail.com>
2022-09-11 09:10:53 -0700

    modified again 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
 int main(void)
 {
+	/* prints using puts */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
