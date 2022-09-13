ac729fb79e127766964a52d194f113d83acffc9c
Pangani <roypangani@gmail.com>
2022-09-12 19:35:30 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
