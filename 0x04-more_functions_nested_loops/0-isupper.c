8b00952943c96a712a9d9555d25da5d8d8d1aefb
Pangani <roypangani@gmail.com>
2022-09-15 02:19:43 -0700

    return 1 if character is uppcase and 0 otherwise

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * isupper - check description
+ * @cLetter: input character
+ * Description: the function checks for upper case character
+ * Return: 1 if uppercase and 0 if lowercase
+ */
+int _isupper(int cLetter)
+{
+	char i, j;
+	int rValue;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (j = 'A'; j <= 'Z'; j++)
+		{
+			if (cLetter == j)
+			{
+				rValue = 1;
+				break;
+			}
+			else if (cLetter == i)
+			{
+				rValue = 0;
+				break;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	return (rValue);
+}
