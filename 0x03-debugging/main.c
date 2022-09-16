2b830b730cc4012f3d785adbe2f96d540d95c64e
Pangani <roypangani@gmail.com>
2022-09-15 19:58:45 -0700

    initial commit

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests functionthat prints if integer is positive oe neg
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
