c20dd6822a6954d686be554b985a254e9861b87f
Pangani <roypangani@gmail.com>
2022-09-15 19:22:58 -0700

    5 more numbers modified

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,13 @@ void more_numbers(void)
 {
 	int count, i;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count < 10; count++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
 				_putchar(i / 10 + '0');
+			
 			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
