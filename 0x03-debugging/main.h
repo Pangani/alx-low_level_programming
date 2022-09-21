2507b7305555a0f58b2a89c907213c8b699b7f8d
Pangani <roypangani@gmail.com>
2022-09-21 06:25:31 -0700

    prints the largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 void positive_or_negative(int i);
-void largest_number(int, int, int);
+int largest_number(int, int, int);
 int convert_dat(int month, int day);
 #endif
