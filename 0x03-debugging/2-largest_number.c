8f11e7048dbb55c7e5063672589958cfa7974afb
Pangani <roypangani@gmail.com>
2022-09-21 06:45:28 -0700

    done task 0

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first number
@@ -7,7 +6,6 @@
  * @c: third number
  * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
 	int largest;
