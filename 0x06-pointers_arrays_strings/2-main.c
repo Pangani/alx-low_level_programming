b8ba09af0e156115ca978223a29201313f2ea4f1
Pangani <roypangani@gmail.com>
2022-09-22 18:48:39 -0700

    string copy

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
  * main - check code
  *
@@ -23,7 +23,6 @@ int main(void)
 	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
 	printf("%s", s1);
 	printf("%s", ptr);
-
 	for (i = 0; i < 98; i++)
 	{
 		if (i % 10)
