fe4cdedbc08551b1dab8ee73afc2d6b1684cf831
Pangani <roypangani@gmail.com>
2022-10-04 16:53:30 -0700

    returns pointer to allcated qspace

---
+++ 0x0B-malloc_free/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - check code
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *s;
+
+	s = _strdup("ALX SE");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
+}
