9d46876bd499f2a963d30a52ddbfe215cfbb6ed3
Pangani <roypangani@gmail.com>
2022-10-10 13:02:45 -0700

    Defining the new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - function for dog object
+ * @name: name
+ * @owner: owner
+ * @age: age
+ */
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} dog;
+#endif
