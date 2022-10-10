0ff3dbaaef3dbe9d92d07a7e8370f35eb5102124
Pangani <roypangani@gmail.com>
2022-10-10 13:48:48 -0700

    modified init.c file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,10 +6,12 @@
  * @name: name
  * @age: age
  * @owner: owner
- * Return: return a structure
+ * Return: nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->owner = owner;
 	d->age = age;
