f7d920e5c2ffbc299d890140fb9da22ded726360
Pangani <roypangani@gmail.com>
2022-10-27 16:10:31 -0700

    prints the binary version of number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - function prints a converted binary
+ * @n: integer to be converted
+ * Return: integer
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int flag = 0, max = 32768;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (max)
+	{
+		if (flag == 1 && (n & max) == 0)
+			_putchar('0');
+		else if ((n & max) != 0)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		max >>= 1;
+	}
+}
